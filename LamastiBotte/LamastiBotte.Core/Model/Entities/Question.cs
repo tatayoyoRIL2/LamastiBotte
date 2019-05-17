647e3531da87d2ca949868a641645c223dee5465
omegaBionic <julien.boully721@gmail.com>
2019-05-17 15:04:25 +0200

    ADD : Documentation pour les classes question et réponse

--- LamastiBotte/LamastiBotte.Core/Model/Entities/Question.cs
+++ LamastiBotte/LamastiBotte.Core/Model/Entities/Question.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace LamastiBotte.Core.Model.Entities
 {
+    /// <summary>
+    /// Créer une <see cref="Question"/>
+    /// </summary>
     public class Question
     {
+        /// <summary>
+        /// Identifiant unique de la question
+        /// </summary>
         public uint Id { get; set; }
+
+        /// <summary>
+        /// String de la question
+        /// </summary>
         public string QuestionString { get; set; }
+
+        /// <summary>
+        /// Liaison entre la question et réponse
+        /// </summary>
         public List<Reponse> reponses = new List<Reponse>();
     }
 }
--- LamastiBotte/LamastiBotte.Core/Model/Entities/Reponse.cs
+++ LamastiBotte/LamastiBotte.Core/Model/Entities/Reponse.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace LamastiBotte.Core.Model.Entities
 {
+    /// <summary>
+    /// Créer une <see cref="Reponse"/>
+    /// </summary>
     public class Reponse
     {
+        /// <summary>
+        /// Identifiant unique de la reponse
+        /// </summary>
         public uint Id { get; set; }
+
+        /// <summary>
+        /// String de la reponse
+        /// </summary>
         public string ReponseString { get; set; }
+
+        /// <summary>
+        /// Liaison entre la réponse et la question
+        /// </summary>
         public List<Question> Questions = new List<Question>();
     }
 }
