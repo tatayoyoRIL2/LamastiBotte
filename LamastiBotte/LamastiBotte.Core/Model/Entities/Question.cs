5a81b1a72051d782e024d8d8a5814d32468aaa07
omegaBionic <julien.boully721@gmail.com>
2019-05-17 14:23:22 +0200

    ADD : Classes question et Réponse

---
+++ LamastiBotte/LamastiBotte.Core/Model/Entities/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamastiBotte.Core.Model.Entities
+{
+    public class Question
+    {
+        public uint id { get; set; };
+        public string question { get; set; };
+
+    }
+}
