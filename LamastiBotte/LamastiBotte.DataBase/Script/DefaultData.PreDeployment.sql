ed2dd54c9455cda45d2109397cd2512505ef37c4
tatayoyo <y.tata@ymag.fr>
2019-05-17 16:43:04 +0200

    ADD  : Scripte de déployement de données par défaut

---
+++ LamastiBotte/LamastiBotte.DataBase/Script/DefaultData.PreDeployment.sql
@@ -0,0 +1,11 @@
+﻿/*
+ Modèle de script de pré-déploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront exécutées avant le script de compilation.	
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de pré-déploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de pré-déploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
