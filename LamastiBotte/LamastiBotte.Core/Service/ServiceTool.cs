2e9c7f2fcc09daf7ed39460772454bab383e4dca
omegaBionic <julien.boully721@gmail.com>
2019-05-17 16:53:15 +0200

    ADD : Commentaires sur la classe ServiceTool

--- LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
+++ LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
@@ -7,15 +7,27 @@ using System.Threading.Tasks;
 
 namespace LamastiBotte.Core.Service
 {
+    /// <summary>
+    /// Créer un <see cref="ServiceTool"/>
+    /// </summary>
     public static class ServiceTool
     {
         public static string FormatMessage(string inputMessage)
         {
+            /// <summary>
+            /// Math avec un regex les caractères "^0-9a-zA-Z "
+            /// </summary>
             string pattern = "";
             string outputMessage = Regex.Replace(inputMessage, @"[^0-9a-zA-Z ]+", "");
 
+            /// <summary>
+            /// Met le message de sortie en lower case
+            /// </summary>
             outputMessage = outputMessage.ToLower();
 
+            /// <summary>
+            /// Supprime les espaces en début et fin de outputMessage
+            /// </summary>
             char[] trimChars = { ' ' };
             outputMessage = outputMessage.TrimEnd(trimChars);
             outputMessage = outputMessage.TrimStart(trimChars);
