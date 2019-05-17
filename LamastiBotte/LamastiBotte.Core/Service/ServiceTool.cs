e92cb19ef6c9ec0114c527c9882a7261aa1cb628
omegaBionic <julien.boully721@gmail.com>
2019-05-17 16:46:30 +0200

    ADD : Full FormatMessageTest

--- LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
+++ LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
@@ -11,8 +11,14 @@ namespace LamastiBotte.Core.Service
     {
         public static string FormatMessage(string inputMessage)
         {
-            string pattern = "[A-Za-z ]*";
-            string outputMessage = Regex.Match(inputMessage, pattern).Value;
+            string pattern = "";
+            string outputMessage = Regex.Replace(inputMessage, @"[^0-9a-zA-Z ]+", "");
+
+            outputMessage = outputMessage.ToLower();
+
+            char[] trimChars = { ' ' };
+            outputMessage = outputMessage.TrimEnd(trimChars);
+            outputMessage = outputMessage.TrimStart(trimChars);
 
             return outputMessage;
         }
