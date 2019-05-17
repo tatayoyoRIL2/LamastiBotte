81bb13a7227a421e52a696b988b5b271dfb04410
omegaBionic <julien.boully721@gmail.com>
2019-05-17 16:16:27 +0200

    ADD : Regex math pour FormatMessage

--- LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
+++ LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LamastiBotte.Core.Service
 {
-    public class ServiceTool
+    public static class ServiceTool
     {
-        public string FormatMessage(string inputMessage)
+        public static string FormatMessage(string inputMessage)
         {
-            return inputMessage;
+            string pattern = "[A-Za-z ]*";
+            string outputMessage = Regex.Match(inputMessage, pattern).Value;
+
+            return outputMessage;
         }
     }
 }
