d2644ceed7bebfcca6ded65672104360e038571e
omegaBionic <julien.boully721@gmail.com>
2019-07-02 14:29:37 +0200

    Cleaned main.

--- LamastiBotte/LamastiBotte.Core/Program.cs
+++ LamastiBotte/LamastiBotte.Core/Program.cs
@@ -9,14 +9,8 @@ using System.Threading.Tasks;
 
 namespace LamastiBotte.Core
 {
-    /// <summary>
-    /// Point d'entrée de lancement de l'application.
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// Méthode principal.
-        /// </summary>
         public static void Main(string[] args)
         {
             string strConnexion = "Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
@@ -32,7 +26,6 @@ namespace LamastiBotte.Core
                 accueilVue vue = new accueilVue();
                 vue.ShowDialog();
                 vue.Dispose();
-
 
                 // BDD : Fermeture de la connection à la BDD.
                 oConnection.Close();
