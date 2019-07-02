a9431d65b37a9c440ed66f07bd9c34f0001ffa6b
omegaBionic <julien.boully721@gmail.com>
2019-07-02 15:43:36 +0200

    Revert "Cleaned main."
    This reverts commit d2644ceed7bebfcca6ded65672104360e038571e.

--- LamastiBotte/LamastiBotte.Core/Program.cs
+++ LamastiBotte/LamastiBotte.Core/Program.cs
@@ -9,8 +9,14 @@ using System.Threading.Tasks;
 
 namespace LamastiBotte.Core
 {
+    /// <summary>
+    /// Point d'entrée de lancement de l'application.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Méthode principal.
+        /// </summary>
         public static void Main(string[] args)
         {
             string strConnexion = "Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
@@ -26,6 +32,7 @@ namespace LamastiBotte.Core
                 accueilVue vue = new accueilVue();
                 vue.ShowDialog();
                 vue.Dispose();
+
 
                 // BDD : Fermeture de la connection à la BDD.
                 oConnection.Close();
