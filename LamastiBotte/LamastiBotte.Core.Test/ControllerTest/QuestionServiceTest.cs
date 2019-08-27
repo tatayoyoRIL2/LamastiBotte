5fe7b70aabaaee912a9769a87b912f0bebaa0615
omegaBionic <julien.boully721@gmail.com>
2019-08-27 11:38:54 +0200

    ADD : SendResponseTests.

--- LamastiBotte/LamastiBotte.Core.Test/ControllerTest/QuestionServiceTest.cs
+++ LamastiBotte/LamastiBotte.Core.Test/ControllerTest/QuestionServiceTest.cs
@@ -1,22 +1,40 @@
 ﻿using LamastiBotte.Core.Service;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace LamastiBotte.Core.Test.ControllerTest
 {
     public class QuestionServiceTest
     {
+        public List<string> questionsList;
+        public List<string> reponseList;
+
         [SetUp]
         public void Setup()
         {
+            questionsList = new List<string> { "bonjour", "je vous écoute" };
+            reponseList = new List<string> { "salut", "tu m'écoute" };
         }
 
         [Test]
-        public void SendResponseTest()
+        public void SendResponseTestEmpty()
         {
-            Assert.AreEqual("Je n'ai pas compris ta question !",
-                QuestionService.SendResponse("&8&8&9895656166656s6dqds16qsd&édazdazdza"));
-            Assert.AreEqual("Ta réponse est vide !",
-                QuestionService.SendResponse(""));
+            Assert.AreEqual(questionsList[1],
+                QuestionService.SendResponse("", questionsList, reponseList));
+        }
+
+        [Test]
+        public void SendResponseTestBadWord()
+        {
+            Assert.AreEqual(questionsList[1],
+                QuestionService.SendResponse("$!yoann! aime !aller au wc456456654!!", questionsList, reponseList));
+        }
+
+        [Test]
+        public void SendResponseTestReponse()
+        {
+            Assert.AreEqual(questionsList[0],
+                QuestionService.SendResponse(reponseList[0], questionsList, reponseList));
         }
     }
 }
