125873a163c7ef73ef297e77ddbe4e57a2ebf401
tatayoyo <y.tata@ymag.fr>
2019-08-27 16:44:03 +0200

    ADD : Spitch

--- LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
+++ LamastiBotte/LamastiBotte.Core/Service/ServiceTool.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Speech.Synthesis;
 using System.Text.RegularExpressions;
 
 namespace LamastiBotte.Core.Service
