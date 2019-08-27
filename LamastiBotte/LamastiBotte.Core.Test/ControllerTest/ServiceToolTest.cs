dbd2a259ed0b4980b32f5f2aa23b7907f1138218
omegaBionic <julien.boully721@gmail.com>
2019-08-27 09:28:20 +0200

    ADD : FormatOutputMessageTest.

--- LamastiBotte/LamastiBotte.Core.Test/ControllerTest/ServiceToolTest.cs
+++ LamastiBotte/LamastiBotte.Core.Test/ControllerTest/ServiceToolTest.cs
@@ -14,12 +14,23 @@ namespace Tests
         public void FormatMessageTest()
         {
             Assert.AreEqual("hello", ServiceTool.FormatMessage("HELLO"));
-            Assert.AreEqual("hello", ServiceTool.FormatMessage("HELLO§"));
+            Assert.AreEqual("hello", ServiceTool.FormatMessage("HELLOï¿½"));
             Assert.AreEqual("hello", ServiceTool.FormatMessage("HELLO!!!!"));
             Assert.AreEqual("hello le monde", ServiceTool.FormatMessage("HELLO LE MONDE !!!!"));
             Assert.AreEqual("hello le monde", ServiceTool.FormatMessage("   HELLO LE MONDE!!!!"));
             Assert.AreEqual("hello le monde", ServiceTool.FormatMessage("   HELLO LE MONDE   !!!!"));
             Assert.AreEqual("hello le monde", ServiceTool.FormatMessage("   HELLO LE MONDE   "));
         }
+
+        [Test]
+        public void FormatOutputMessageTest()
+        {
+            Assert.AreEqual("Je suis d'accord.", ServiceTool.FormatOutputMessage("je suis d'accord"));
+            Assert.AreEqual("Tristan aime les fleurs.", ServiceTool.FormatOutputMessage("tristan aime les fleurs"));
+            Assert.AreEqual("Coucou.", ServiceTool.FormatOutputMessage("coucou"));
+            Assert.AreEqual("A.", ServiceTool.FormatOutputMessage("a"));
+            Assert.AreEqual("A.", ServiceTool.FormatOutputMessage("a"));
+        }
+
     }
 }
